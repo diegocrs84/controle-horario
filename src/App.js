9fd9a80c09429ece5a1d4729640cc02a10b7c432
diegocrs84 <dcaninde@yahoo.com.br>
2025-03-06 14:41:04 -0300

    Alteração de logo do app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ControleHoras from "./Controlehoras";
+import logohorario from "./images";
 import "./ControleHoras.css";
 
 function App() {
-  return (
-    <div>
-      <ControleHoras />
-    </div>
-  );
+return (
+  <div className="App">
+    <header className="App-header">
+      <img src={logohorario} alt="Controle Horas" width="150" />
+      <h1>ControleHoras</h1>
+    </header>
+  </div>
+);
 }
 
 export default App;
